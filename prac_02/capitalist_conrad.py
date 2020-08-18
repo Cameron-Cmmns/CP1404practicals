63727fc28c7f084c49205eb7f5c6f9b1bf0c7749
Cameron Cummins <cameron.cummins@my.jcu.edu.au>
2020-08-19 09:40:01 +1000

    Write to file

--- prac_02/capitalist_conrad.py
+++ prac_02/capitalist_conrad.py
@@ -9,14 +9,19 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 17.5  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.00
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+OUTPUT_FILE = "price_and_day_record"
 
 price = INITIAL_PRICE
-print("${:,.2f}".format(price))
+day_count = 0
+
+out_file = open(OUTPUT_FILE, 'w')
+
+print("${:,.2f}".format(price), file=out_file)
 
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
@@ -24,12 +29,16 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
         # generate a random floating-point number
-        # between 0 and MAX_INCREASE
+        # between 0.1 and MAX_INCREASE
         price_change = random.uniform(0, MAX_INCREASE)
     else:
         # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
+        # between negative MAX_DECREASE and 17.5
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print("${:,.2f}".format(price))
+    print("On day {} price is ${:,.2f}".format(day_count, price))
+    day_count += 1
+
+out_file.close()
+
