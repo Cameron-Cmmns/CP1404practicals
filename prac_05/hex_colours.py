4b7bbdb520afa8514a83b9712054726b8b0de0f6
Cameron Cummins <cameron.cummins@my.jcu.edu.au>
2020-09-02 13:47:20 +1000

    Complete hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,1 +1,20 @@
-.
+"""CP1404 - Practicals - prac_05"""
+
+COLOUR_NAMES = {"ALICEBLUE": "#f0f8ff", "ANTIQUEWHITE": "#faebd7", "AQUAMARINE1": "#7fffd4",
+                "AZURE1": "#f0ffff", "BEIGE": "#f5f5dc", "BISQUE": "#ffe4c4",
+                "BLACK": "#000000", "BLANCHEDALMOND": "#ffebcd", "BLUE1": "#0000ff",
+                "BlueViolet": "#8a2be2"}
+print(COLOUR_NAMES)
+
+colour_code = input("Enter colour name: ").upper()
+
+while colour_code != "":
+    try:
+        if colour_code in COLOUR_NAMES:
+            print(COLOUR_NAMES[colour_code])
+        else:
+            print("Invalid colour name")
+        colour_code = input("Enter colour name: ").upper()
+    except ValueError:
+        print("Invalid")
+
