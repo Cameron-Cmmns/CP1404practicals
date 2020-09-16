e0bc8f4a930a0e26e884a32a4ca6fb22eac06781
Cameron Cummins <cameron.cummins@my.jcu.edu.au>
2020-09-17 08:32:04 +1000

    Create file

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,40 +2,6 @@
    Emails
 """
 
-# email_dict = {}
-#
-# user_input = input("Enter Email: ")
-#
-# name = ""
-# user_email = ""
-#
-#
-# emails = user_input.split()
-# where_to_cut = user_input.find("@")
-#
-# while user_input != "":
-#
-#     for email in emails:
-#         user_email = email
-#         name = email_dict.get(email, user_input[0:where_to_cut])
-#         email_dict[email] = name
-#
-#         check_is_name = input("Is your name {}? (Y/n) ".format(name))
-#         check_is_name.lower()
-#         if check_is_name == "y":
-#             email_dict[email] = name
-#         elif check_is_name == "n":
-#             new_name = input("Name: ")
-#             email_dict[email] = new_name
-#         else:
-#             print("Invalid input")
-#
-#     user_input = input("Enter Email: ")
-#
-# for name in emails:
-#     print("{} ({})".format(email_dict[name], user_email))
-
-
 def main():
     email_to_name = {}
 
--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,24 +2,6 @@
     Word Occurrences
 """
 
-# example:
-# word_to_count = {}
-# text = input("Text: ")
-# words = text.split()
-#
-# for word in words:
-#     frequency = word_to_count.get(word, 0)
-#     word_to_count[word] = frequency + 1
-#
-# words = list(word_to_count.keys())
-# words.sort()
-#
-# max_length = max(len(word) for word in words)
-# for word in words:
-#     print("{:{}} : {}".format(word, max_length, word_to_count[word]))
-
-# ----
-
 
 # Large portion copied off the example; previous attempts didn't work.
 # had to use the example for this.
