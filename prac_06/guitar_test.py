e6ca5e660baa9a1ecddbd3474074143f7cfc8316
Cameron Cummins <cameron.cummins@my.jcu.edu.au>
2020-09-17 18:06:12 +1000

    Add age testing

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,8 +1,13 @@
 from prac_06.guitar import Guitar
 
 gibson = Guitar("Gibson L-5", 1922, 16035.40)
+another = Guitar("Another Guitar", 2013, 200)
 
-age = gibson.get_age()
-print(age)
+age_guitar = gibson.get_age()
+age_another_guitar = another.get_age()
+
+print("{} get_age() - Expected 98. Got {}".format(gibson.name, age_guitar))
+print("{} get_age() - Expected 7. Got {}".format(another.name, age_another_guitar))
+
 
 print(gibson)
